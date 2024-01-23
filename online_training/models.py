6c9ba6a23e1672251b38a1d202f4abc4444f5232
AleksandrStrulkov <astrulkov@yandex.ru>
2024-01-23 22:17:19 +0300

    Adding exercise 2

--- online_training/models.py
+++ online_training/models.py
@@ -5,6 +5,12 @@ from django.contrib.auth import get_user_model
 
 NULLABLE = {'blank': True, 'null': True}
 
+PAY_METHOD = (
+		(None, 'Выберите способ оплаты'),
+		('Cash', 'Наличные'),
+		('Transfer', 'Перевод')
+)
+
 
 class Course(models.Model):
 	title = models.CharField(max_length=150, verbose_name='Название курса')
@@ -31,9 +37,9 @@ class Lesson(models.Model):
 	preview = models.ImageField(upload_to='images_lesson/', verbose_name='Превью', **NULLABLE)
 	video = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
 	course = models.ForeignKey(
-		Course, verbose_name='Курс', on_delete=models.CASCADE, **NULLABLE,
-		related_name="lesson"
-		)
+			Course, verbose_name='Курс', on_delete=models.CASCADE, **NULLABLE,
+			related_name="lesson"
+	)
 	owner = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, **NULLABLE)
 
 	def __str__(self):
@@ -42,6 +48,32 @@ class Lesson(models.Model):
 	class Meta:
 		verbose_name = 'урок'
 		verbose_name_plural = 'уроки'
+
+	@classmethod
+	def truncate_table_restart_id(cls):
+		with connection.cursor() as cursor:
+			cursor.execute(f'ALTER SEQUENCE catalog_statusproduct_id_seq RESTART WITH 1;')
+
+
+class Payments(models.Model):
+	user = models.ForeignKey(
+		get_user_model(), on_delete=models.SET_NULL, verbose_name='Пользователь', **NULLABLE,
+		related_name="payments"
+		)
+	date_payment = models.DateField(verbose_name='Дата оплаты')
+	payment_course = models.ForeignKey(
+		Course, verbose_name='Оплаченный курс', on_delete=models.CASCADE, **NULLABLE,
+		related_name="payments"
+		)
+	payment_amount = models.IntegerField(verbose_name='Сумма оплаты')
+	payment_method = models.CharField(choices=PAY_METHOD, default='Cash', max_length=10, verbose_name='Способ оплаты')
+
+	def __str__(self):
+		return f'{self.user} - {self.date_payment}'
+
+	class Meta:
+		verbose_name = 'оплата'
+		verbose_name_plural = 'оплаты'
 
 	@classmethod
 	def truncate_table_restart_id(cls):
--- online_training/urls.py
+++ online_training/urls.py
@@ -4,12 +4,14 @@ from rest_framework import routers
 from online_training.apps import OnlineTrainingConfig
 from online_training.views.course import CourseViewSet
 from online_training.views.lesson import APILessonDetail, APILesson
+from online_training.views.payments import APIPayments
 
 app_name = OnlineTrainingConfig.name
 
 urlpatterns = [
 	path('api/lesson/', APILesson.as_view()),
 	path('api/lesson/<int:pk>/', APILessonDetail.as_view()),
+	path('api/payments/', APIPayments.as_view())
 ]
 
 router = routers.SimpleRouter()
