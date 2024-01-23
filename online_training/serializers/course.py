c096f64161e704a59192d91be60fec00e5ad5c96
AleksandrStrulkov <astrulkov@yandex.ru>
2024-01-23 22:42:24 +0300

    Adding exercise 3

--- online_training/serializers/course.py
+++ online_training/serializers/course.py
@@ -2,10 +2,15 @@ from rest_framework import serializers
 
 from online_training.models import Course, Lesson
 from online_training.serializers.lesson import LessonSerializer
+from rest_framework.relations import SlugRelatedField
+
+from users.models import User
 
 
 class CourseSerializer(serializers.ModelSerializer):
 	lessons_count = serializers.SerializerMethodField()
+	lesson = LessonSerializer(read_only=True, many=True)
+	owner = SlugRelatedField(slug_field='email', queryset=User.objects.all())
 
 	def get_lessons_count(self, obj):
 		if obj.lesson.all():
--- online_training/serializers/lesson.py
+++ online_training/serializers/lesson.py
@@ -1,9 +1,12 @@
 from rest_framework import serializers
 
-from online_training.models import Lesson
+from online_training.models import Lesson, Course
+from rest_framework.relations import SlugRelatedField
 
 
 class LessonSerializer(serializers.ModelSerializer):
+	course = SlugRelatedField(slug_field='title', queryset=Course.objects.all())
+
 	class Meta:
 		model = Lesson
 		fields = '__all__'
