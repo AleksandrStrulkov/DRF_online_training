12378176e224311b87085288e7412fb825397206
AleksandrStrulkov <astrulkov@yandex.ru>
2024-01-23 06:56:20 +0300

    Adding exercise 1 (HW 24.2)

--- online_training/serializers/course.py
+++ online_training/serializers/course.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
-from online_training.models import Course
+from online_training.models import Course, Lesson
+from online_training.serializers.lesson import LessonSerializer
 
 
 class CourseSerializer(serializers.ModelSerializer):
+	lessons_count = serializers.SerializerMethodField()
+
+	def get_lessons_count(self, obj):
+		if obj.lesson.all():
+			return obj.lesson.all().count()
+		return 0
+
 	class Meta:
 		model = Course
 		fields = '__all__'
