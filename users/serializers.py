68b22414996f7f73c56cbf1904d84049fc2f0e74
AleksandrStrulkov <astrulkov@yandex.ru>
2024-01-24 15:20:13 +0300

    Adding additional task

--- users/serializers.py
+++ users/serializers.py
@@ -1,9 +1,17 @@
 from rest_framework import serializers
 
+from online_training.serializers.payments import PaymentsSerializer
 from users.models import User
 
 
 class UserSerializer(serializers.ModelSerializer):
+	payments = PaymentsSerializer(many=True, read_only=True)
+
 	class Meta:
 		model = User
-		fields = ("id", "first_name", "email",)
+		fields = ("id", "first_name", "email", "payments",)
+
+
+
+
+
