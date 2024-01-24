794f70b84cf6cd2f506eb5dd2b020f028f10f439
AleksandrStrulkov <astrulkov@yandex.ru>
2024-01-24 14:50:07 +0300

    Adding exercise 4

--- online_training/views/lesson.py
+++ online_training/views/lesson.py
@@ -2,11 +2,15 @@ from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIV
 
 from online_training.models import Lesson
 from online_training.serializers.lesson import LessonSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter
 
 
 class APILesson(ListCreateAPIView):
 	queryset = Lesson.objects.all()
 	serializer_class = LessonSerializer
+	filter_backends = [DjangoFilterBackend, OrderingFilter]
+	filterset_fields = ('title', 'course',)
 
 
 class APILessonDetail(RetrieveUpdateDestroyAPIView):
--- online_training/views/payments.py
+++ online_training/views/payments.py
@@ -1,9 +1,13 @@
-from rest_framework.generics import ListCreateAPIView
-
+from rest_framework.generics import ListAPIView
 from online_training.models import Payments
 from online_training.serializers.payments import PaymentsSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import OrderingFilter, SearchFilter
 
 
-class APIPayments(ListCreateAPIView):
+class APIPayments(ListAPIView):
 	queryset = Payments.objects.all()
 	serializer_class = PaymentsSerializer
+	filter_backends = [DjangoFilterBackend, OrderingFilter]
+	filterset_fields = ('payment_course', 'payment_method',)
+	ordering_fields = ('date_payment',)
