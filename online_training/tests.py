728721be5fdabcf3bb5f47e70f42d69f9fecad0b
AleksandrStrulkov <astrulkov@yandex.ru>
2024-02-01 01:43:40 +0300

    Adding exercise-4 HW-25.2

--- online_training/tests.py
+++ online_training/tests.py
@@ -15,26 +15,25 @@ class LessonTestCase(APITestCase):
 		self.client.force_authenticate(user=self.user)
 
 		self.course = Course.objects.create(
-				title='Test course',
-				description='Test description',
-				owner=self.user
+		title='Test course',
+		description='Test description',
+		owner=self.user
 		)
 
 		self.lesson = Lesson.objects.create(
-				title='Test lesson',
-				description='Test description',
-				course=self.course,
-				owner=self.user,
+		title='Test lesson',
+		description='Test description',
+		course=self.course,
+		owner=self.user,
 		)
 
 	def test_create_lesson_(self):
 		"""Тестирование создания уроков"""
 		data = {
-				'title': 'test lesson',
-				'description': 'test description',
-				'video': 'https://www.youtube.com/watch?v=bITQ13XCU9Q&list=PLA0M1Bcd0w8xZA3Kl1fYmOH_MfLpiYMRs&index=15',
-				'course': self.course
-
+		'title': 'test lesson',
+		'description': 'test description',
+		'video': 'https://www.youtube.com/watch?v=bITQ13XCU9Q&list=PLA0M1Bcd0w8xZA3Kl1fYmOH_MfLpiYMRs&index=15',
+		'course': self.course
 		}
 		response = self.client.post('/api/lesson/', data=data)
 		print(response.json())
@@ -54,11 +53,11 @@ class LessonTestCase(APITestCase):
 
 		url = f'/api/lesson/{self.lesson.id}/'
 		response = self.client.patch(url, {'title': 'new title test',
-											'description': 'new description test',
-											'course': self.course,
-											'video': 'https://www.youtube.com/watch?v=fTLOX6x-GQg&t=101s'
-											}
-									 )
+		'description': 'new description test',
+		'course': self.course,
+		'video': 'https://www.youtube.com/watch?v=fTLOX6x-GQg&t=101s'
+		}
+		)
 		self.lesson.refresh_from_db()
 
 		self.assertEqual(response.status_code, status.HTTP_200_OK)
@@ -82,30 +81,36 @@ class SubscriptionTestCase(APITestCase):
 		self.client.force_authenticate(user=self.user)
 
 		self.course = Course.objects.create(
-				title='Test course',
-				description='Test description',
-				owner=self.user
+		title='Test course',
+		description='Test description',
+		owner=self.user
 		)
 
 		self.subscription = Subscription.objects.create(
-			user=self.user,
-			course=self.course,
-			is_active=True
+		user=self.user,
+		course=self.course,
+		is_active=True
 		)
 
 	def test_create_subscription(self):
 		""" Тестирование создания подписки на курс """
 
 		data = {
-				'user': self.user.id,
-				'course': self.course.id,
-				'is_active': True
+		'user': self.user.id,
+		'course': self.course.id,
+		'is_active': True
 		}
 		response = self.client.post('/subscription/', data=data)
 		print(response.json())
 		self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 		self.assertTrue(Subscription.objects.filter(course=self.course.id, user=self.user.id, is_active=True))
-		self.assertEqual(response.json(), {'id': 2, 'is_active': True, 'user': self.user.id, 'course': self.course.id})
+		self.assertEqual(response.json(), {
+		'id': 2,
+		'is_active': True,
+		'user': self.user.id,
+		'course': self.course.id
+		}
+		)
 
 	def test_delete_subscription(self):
 		""" Тестирование удаления подписки на курс"""
@@ -114,8 +119,8 @@ class SubscriptionTestCase(APITestCase):
 
 		self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
 		self.assertFalse(Subscription.objects.filter(user=self.user.id,
-													course=self.course.id,
-													is_active=True))
+		course=self.course.id,
+		is_active=True))
 
 
 
